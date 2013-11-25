3bc40b9c04d7b236a16a447e57e4ea75a48d767f
Dustin Shahidehpour <dshahidehpour@gmail.com>
2013-11-25 14:11:40 -0600

    Incorporated flask_frozen, now generates pages.

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
+
+import sys
+import shutil
 
 app = Flask(__name__)
+freezer = Freezer(app)
 
 ########################################
 ############  App URLs  ################
@@ -12,25 +17,29 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/introduction")
+@app.route("/introduction/")
 def introduction():
     return render_template('introduction.html')
 
 
-@app.route("/materials")
+@app.route("/materials/")
 def materials():
     return render_template('materials.html')
 
 
-@app.route("/results")
+@app.route("/results/")
 def results():
     return render_template('results.html')
 
 
-@app.route("/conclusion")
+@app.route("/conclusion/")
 def conclusion():
     return render_template('conclusion.html')
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        shutil.rmtree('./build')
+        freezer.freeze()
+    else:
+        app.run(host='0.0.0.0', debug=True)
